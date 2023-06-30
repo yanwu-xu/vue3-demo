7d103634f07884d03dac53886b994c1ba4e42104
ljh <1782877288@qq.com>
2023-06-30 11:14:00 +0800

    feat： 新增 pageBack 返回组件

---
+++ src/components/PageBack/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="page-back">
+    <span class="page-back-inner" @click="goBack">
+      <i class="el-icon-back"></i>
+      <span class="ml-15">{{ text || '返回' }}</span>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts" name="PageBack">
+import { useAttrs } from 'vue'
+import { useRouter } from 'vue-router'
+
+const attrs = useAttrs()
+const router = useRouter()
+const emit = defineEmits<{
+  (event: 'goBack'): void
+}>()
+defineProps<{
+  text: string
+}>()
+
+const goBack = () => {
+  if (!attrs.goBack || typeof attrs.goBack !== 'function') {
+    router.back()
+  } else {
+    emit('goBack')
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.page-back {
+  .page-back-inner {
+    i {
+      font-size: 18px;
+      cursor: pointer;
+    }
+
+    span {
+      font-size: 18px;
+      cursor: pointer;
+    }
+
+    &:hover {
+      i {
+        color: $mainColor;
+      }
+
+      span {
+        color: $mainColor;
+      }
+    }
+  }
+}
+</style>
