0d5041eff267f78b18deb70e700d975e752bd208
ljh <1782877288@qq.com>
2023-07-03 09:23:04 +0800

    fix: 修复ts text-over 中ts 警告错误

--- src/components/TextOver/index.vue
+++ src/components/TextOver/index.vue
@@ -1,47 +1,61 @@
 <script setup lang="ts" name="TextOver">
-import { ref, watch, nextTick } from 'vue'
-const props = withDefaults(defineProps<{
-  text: string | number,
-  emptyValue: string | number
-}>(), {
-  text: '',
-  emptyValue: '--'
-})
+  import { ref, watch, nextTick } from 'vue'
+  const props = withDefaults(
+    defineProps<{
+      text: string | number
+      emptyValue: string | number
+    }>(),
+    {
+      text: '',
+      emptyValue: '--',
+    }
+  )
 
-const isOver = ref<boolean>(false)
-const textWrap = ref(null)
+  const isOver = ref<boolean>(false)
+  const textWrap = ref<HTMLDivElement | null>(null)
 
-watch(() => props.text, (text, prevText) => {
-  nextTick(() => {
-    handleOver()
-  })
-}, {
-  immediate: true
-})
+  watch(
+    () => props.text,
+    () => {
+      nextTick(() => {
+        handleOver()
+      })
+    },
+    {
+      immediate: true,
+    }
+  )
 
-const handleOver = () => {
-  const target = textWrap.value
-  const offsetWidth = target.offsetWidth
-  const scrollWidth = target.scrollWidth
-  isOver.value = scrollWidth > offsetWidth
-}
+  const handleOver = () => {
+    const target = textWrap.value
+    if (target) {
+      const offsetWidth = target?.offsetWidth
+      const scrollWidth = target?.scrollWidth
+      isOver.value = scrollWidth > offsetWidth
+    }
+  }
 </script>
 
 <template>
-  <el-tooltip effect="light" :disabled="!isOver" :content="text" placement="top-start" :open-delay="600"
-    :visible-arrow="false">
+  <el-tooltip
+    effect="light"
+    :disabled="!isOver"
+    :content="text"
+    placement="top-start"
+    :open-delay="600"
+    :visible-arrow="false"
+  >
     <div class="text-wrap" ref="textWrap">
       {{ text || emptyValue }}
     </div>
   </el-tooltip>
 </template>
 
-
 <style scoped lang="scss">
-.text-wrap {
-  width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
+  .text-wrap {
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 </style>
