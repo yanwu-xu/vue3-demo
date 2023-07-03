785501dccc04c1689e25589c66fb66d97c31fd4d
ljh <1782877288@qq.com>
2023-07-03 09:46:26 +0800

    fix: format demo/page1 page2 page3

--- src/views/demo/page2.vue
+++ src/views/demo/page2.vue
@@ -1,51 +1,9 @@
-<script setup lang="ts" name="demo_page1">
-import { reactive } from 'vue'
-
-const searchForm = reactive({
-  queryFiled1: '11',
-  queryFiled2: '',
-  queryFiled3: '',
-  queryFiled4: ''
-})
-const pagination = reactive<{ pageNum: number, pageSize: number, total: number | string }>({
-  pageNum: 1,
-  pageSize: 10,
-  total: 0
-})
-const handleQuery = (firstpage: string): void => {
-  if (firstpage) pagination.pageNum = 1
-  const params = { ...searchForm, ...pagination }
-  tableData = [
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' },
-    { name: '公司a' }
-  ]
-}
-</script>
+<script setup lang="ts" name="demo_page1"></script>
 
 <template>
-  <Page :pagination="pagination" :table-auto-height="false" show-search-value stretch @query="handleQuery">
+  <Page :table-auto-height="false" stretch>
     <template #left>
-      <div class="left" s-width="200px" s-bg-color="#c2e1f7">
-        page-left
-      </div>
+      <div class="left" s-width="200px" s-bg-color="#c2e1f7"> page-left </div>
     </template>
     <template #right>
       <div class="right" s-width="calc(100% - 200px - 20px)" s-bg-color="#fde689">age-right</div>
--- src/views/demo/page3.vue
+++ src/views/demo/page3.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts" name="demo_page3">
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-const imgUrl = ref('https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100')
+  const imgUrl = ref<string>(
+    'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+  )
 </script>
 
 <template>
   <page>
-    <div style="height: 1000px;border: 1px solid red;"></div>
+    <div style="height: 1000px; border: 1px solid red"></div>
     <div>
       <img v-lazy="imgUrl" alt="" />
     </div>
