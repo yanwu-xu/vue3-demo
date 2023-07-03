2b560bd68678c21a23cbad144c3c8d821a18c8f8
ljh <1782877288@qq.com>
2023-07-03 09:41:00 +0800

    fix: format VideoJs.vue 并 修改ts 类型报错

--- src/components/VideoJs/index.vue
+++ src/components/VideoJs/index.vue
@@ -1,116 +1,121 @@
 <template>
-  <video :key="props.keyId" ref="videoPlayer" id="my-player" :class="{ 'video-js': true, 'full-video': props.fullContainer }" data-setup="{}">
+  <video
+    :key="props.keyId"
+    ref="videoPlayer"
+    id="my-player"
+    :class="{ 'video-js': true, 'full-video': props.fullContainer }"
+    data-setup="{}"
+  >
     <p class="vjs-no-js">
       To view this video please enable JavaScript, and consider upgrading to a web browser that
-      <a href="https://videojs.com/html5-video-support/" target="_blank">
-        supports HTML5 video
-      </a>
+      <a href="https://videojs.com/html5-video-support/" target="_blank"> supports HTML5 video </a>
     </p>
   </video>
 </template>
 
 <script setup lang="ts" name="Videojs">
-import videojs from 'video.js'
-import type Player from 'video.js/dist/types/player'
-import 'video.js/dist/video-js.css'
-import { ref, reactive, useAttrs, onMounted, onBeforeUnmount } from 'vue'
+  import videojs from 'video.js'
+  import 'video.js/dist/video-js.css'
+  import type Player from 'video.js/dist/types/player'
+  import { ref, reactive, useAttrs, onMounted, onBeforeUnmount } from 'vue'
 
-const attrs = useAttrs()
-const props = defineProps({
-  keyId: {
-    type: [String, Number],
-    default: 'player'
-  },
-  fullContainer: {
-    type: Boolean,
-    default: true
-  },
-  // See: [Options Guide](https://docs.videojs.com/tutorial-options.html)
-  option: {
-    type: Object,
-    default: () => { }
-  }
-})
-const emit = defineEmits<{
-  (event: 'callback', player: Player): void
-}>()
-
-let player = ref(null)
-let videoPlayer = ref(null)
-let defaultOptions = reactive({
-  muted: true,
-  loop: true,
-  preload: 'auto',
-  autoplay: true,
-  fluid: true,
-  controls: true,
-  poster: '',
-  sources: [
+  const attrs = useAttrs()
+  const props = withDefaults(
+    defineProps<{
+      keyId: string | number
+      fullContainer: boolean
+      option?: any
+    }>(),
     {
-      src: '//vjs.zencdn.net/v/oceans.mp4',
-      type: 'video/mp4'
+      keyId: 'player',
+      fullContainer: true,
+      option: {}, // See: [Options Guide](https://docs.videojs.com/tutorial-options.html)
     }
-  ]
-})
+  )
+  const emit = defineEmits<{
+    (event: 'callback', player: Player): void
+  }>()
 
-onMounted(() => {
-  initVideoPlayer()
-})
-
-onBeforeUnmount(() => {
-  destoryPlayer()
-  player.value = null
-})
-
-const initVideoPlayer = (): void => {
-  const option = { ...defaultOptions, ...props.option }
-  player.value = videojs(videoPlayer.value, option, function onPlayerReady(): void {
-    if (!attrs.callback && typeof attrs.callback !== 'function') {
-      const callback = function (that) {
-        that.play()
-      }
-      callback(that)
-    } else {
-      emit('callback', player)
-    }
+  let player = ref<Player | null>(null)
+  let videoPlayer = ref<HTMLVideoElement | null>(null)
+  let defaultOptions = reactive({
+    muted: true,
+    loop: true,
+    preload: 'auto',
+    autoplay: true,
+    fluid: true,
+    controls: true,
+    poster: '',
+    sources: [
+      {
+        src: '//vjs.zencdn.net/v/oceans.mp4',
+        type: 'video/mp4',
+      },
+    ],
   })
-}
 
-const pauseVideo = (): void => {
-  player.value.pause()
-}
+  onMounted(() => {
+    initVideoPlayer()
+  })
 
-const updateVideoPlayerSrc = (sources): void => {
-  if (!player.value) return
-  player.value.pause()
-  player.value.src(sources)
-  player.value.load(sources)
-  player.value.play()
-}
+  onBeforeUnmount(() => {
+    destoryPlayer()
+    player.value = null
+  })
 
-const destoryPlayer = (): void => {
-  pauseVideo()
-  player.value && player.value.dispose()
-}
+  const initVideoPlayer = (): void => {
+    const option = { ...defaultOptions, ...props.option }
+    player.value = videojs(
+      videoPlayer.value as HTMLVideoElement,
+      option,
+      function onPlayerReady(): void {
+        if (!attrs.callback && typeof attrs.callback !== 'function') {
+          const callback = function (that) {
+            that.play()
+          }
+          callback(player.value)
+        } else {
+          emit('callback', player.value as Player)
+        }
+      }
+    )
+  }
 
+  const pauseVideo = (): void => {
+    if (!player.value) return
+    player.value.pause()
+  }
 
-defineExpose({
-  player,
-  initVideoPlayer,
-  pauseVideo,
-  updateVideoPlayerSrc,
-  destoryPlayer
-})
+  const updateVideoPlayerSrc = (sources): void => {
+    if (!player.value) return
+    player.value.pause()
+    player.value.src(sources)
+    player.value.load()
+    player.value.play()
+  }
+
+  const destoryPlayer = (): void => {
+    pauseVideo()
+    player.value && player.value.dispose()
+  }
+
+  defineExpose({
+    player,
+    initVideoPlayer,
+    pauseVideo,
+    updateVideoPlayerSrc,
+    destoryPlayer,
+  })
 </script>
 
 <style lang="scss" scoped>
-.video-js {
-  width: 100%;
-  height: 100% !important;
-  cursor: pointer;
-}
+  .video-js {
+    width: 100%;
+    height: 100% !important;
+    cursor: pointer;
+  }
 
-.full-video {
-  object-fit: cover;
-}
+  .full-video {
+    object-fit: cover;
+  }
 </style>
