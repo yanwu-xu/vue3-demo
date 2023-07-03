e8e367b2a2264bead842a157b4f974a5e04fcb40
ljh <1782877288@qq.com>
2023-07-03 17:59:25 +0800

    fix: directives文件 下 js 文件改为ts

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,9 +1,10 @@
+import { type App } from "vue"
 import { store } from '@/store'
 import _ from 'lodash'
 
-const directivesCb = (app, directives = {}) => {
+const directivesCb = (app: App, directives = {}) => {
   const requireDirective = import.meta.globEager('./*/*.js')
-  const _directives = window.env.useMainResources?.includes('directives') ? _.cloneDeep(directives) : {}
+  const _directives = (window as any).env.useMainResources?.includes('directives') ? _.cloneDeep(directives) : {}
 
   for (const path in requireDirective) {
     if (Object.prototype.hasOwnProperty.call(requireDirective, path)) {
