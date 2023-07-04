30a5ccfcc3f0bf696a635c34b802a0a1c0aee748
ljh <1782877288@qq.com>
2023-07-04 10:41:34 +0800

    fix: directives/index.ts 动态导入文件 名错误

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -3,14 +3,14 @@ import { store } from '@/store'
 import _ from 'lodash'
 
 const directivesCb = (app: App, directives = {}) => {
-  const requireDirective = import.meta.globEager('./*/*.js')
+  const requireDirective = import.meta.globEager('./*/*.ts')
   const _directives = (window as any).env.useMainResources?.includes('directives') ? _.cloneDeep(directives) : {}
 
   for (const path in requireDirective) {
     if (Object.prototype.hasOwnProperty.call(requireDirective, path)) {
       const module = requireDirective[path]
       // 处理导入的模块
-      const name = module.default.name ?? path.replace(/\.\/(.*)\/.*\.js/, '$1')
+      const name = module.default.name ?? path.replace(/\.\/(.*)\/.*\.ts/, '$1')
       if (!_directives?.[name]) {
         _directives[name] = module.default || module
       }
