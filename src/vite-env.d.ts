39b9ab62b579c5d2c65cfa55ef4c07fb91ce6c4c
ljh <1782877288@qq.com>
2023-07-04 10:37:08 +0800

    del: 删除 src 下 vite.env.d.ts, types 文件下已经定义了

--- src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-declare module '*vue' {
-  import { ComponentOptions } from "vue"
-  const ComponentOptions: ComponentOptions
-  export default ComponentOptions
-}
